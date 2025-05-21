9b5c595c0fefa023d20cce4da195f5b06aa6ac29
Artem Vasylenko <artemjjke@gmail.com>
2025-05-21 19:57:01 +0300

    host/join menu baseline

--- src/WOTRMultiplayer/Menu/MenuPatches.cs
+++ src/WOTRMultiplayer/Menu/MenuPatches.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
 using HarmonyLib;
@@ -10,6 +11,7 @@ using Kingmaker.UI.MVVM._PCView.MainMenu;
 using Kingmaker.UI.MVVM._VM.ContextMenu;
 using Kingmaker.UI.ServiceWindow;
 using Kingmaker.UI.ServiceWindow.Credits;
+using Owlcat.Runtime.UI.Controls.Button;
 using TMPro;
 using UnityEngine;
 using WOTRMultiplayer.Strings;
@@ -28,7 +30,7 @@ namespace WOTRMultiplayer.Menu
             Logging.Logger.Info("Applying");
             var menuButtons = __instance.transform.GetChild(0);
             var menuItemToCopy = menuButtons.GetChild(3).gameObject;
-            var multiplayerMenu = Object.Instantiate(menuItemToCopy, menuButtons.transform);
+            var multiplayerMenu = UnityEngine.Object.Instantiate(menuItemToCopy, menuButtons.transform);
             multiplayerMenu.transform.SetSiblingIndex(3);
             var multiplayerMenuView = multiplayerMenu.GetComponent<ContextMenuEntityPCView>();
             var window = GetOrCreateMultiplayerWindow();
@@ -44,45 +46,85 @@ namespace WOTRMultiplayer.Menu
                 return _mainMenuMultiplayerWindow;
             }
 
-            var copy = Object.Instantiate(Game.Instance.UI.CreditsUI.gameObject, Game.Instance.UI.MainMenu.transform);
+            var copy = UnityEngine.Object.Instantiate(Game.Instance.UI.CreditsUI.gameObject, Game.Instance.UI.MainMenu.transform);
             var originalWindow = copy.GetComponent<CreditsUIWindow>();
             _mainMenuMultiplayerWindow = copy.AddComponent<MultiplayerWindow>();
-            Object.DestroyImmediate(originalWindow);
-            var creditsScreen = copy.transform.Find("CreditsScreen")?.gameObject;
-            SetupLayout(creditsScreen);
+            UnityEngine.Object.DestroyImmediate(originalWindow);
             _mainMenuMultiplayerWindow.Initialize();
             return _mainMenuMultiplayerWindow;
         }
 
-        private static void SetupLayout(GameObject screen)
+        public class MenuItemController
         {
-            if (screen == null)
+            private const string SelectedGameObjectName = "SelectedImage";
+            private const string HoverGameObjectName = "HoverImage";
+
+            private bool _isInitialized = false;
+            private OwlcatButton _button => UnityObject.gameObject.GetComponent<OwlcatButton>();
+            private GameObject _activeImage;
+            private GameObject _hoverImage;
+
+            public GameObject UnityObject { get; private set; }
+
+            public bool IsActive => UnityObject.gameObject.activeSelf;
+
+            public event EventHandler OnClicked;
+
+            public MenuItemController(GameObject menuItem)
             {
-                Logging.Logger.Error("MP Screen is missing");
-                return;
+                UnityObject = menuItem;
             }
 
-            for (int i = screen.transform.childCount - 1; i >= 0; i--)
+            public void Initialize()
             {
-                if (i == 1)
+                if (_isInitialized)
                 {
-                    continue;
+                    return;
                 }
 
-                Object.DestroyImmediate(screen.transform.GetChild(i).gameObject);
+                _isInitialized = true;
+                _button.OnHover.AddListener(OnHover);
+                _button.OnLeftClick.AddListener(OnClickedInternal);
+                _activeImage = this.UnityObject.transform.Find(SelectedGameObjectName).gameObject;
+                _hoverImage = this.UnityObject.transform.Find(HoverGameObjectName).gameObject;
+
+                Deactivate();
             }
 
-            var labelText = screen.transform.GetComponentInChildren<TextMeshProUGUI>();
-            labelText.SetText(StringsConst.MultiplayerWindow.HostMenuLabel);
+            private void OnHover(bool state)
+            {
+                _hoverImage.SetActive(state);
+            }
+
+            public void Activate()
+            {
+                _activeImage.SetActive(true);
+            }
+
+            public void Deactivate()
+            {
+                _activeImage.SetActive(false);
+            }
+
+            private void OnClickedInternal()
+            {
+                OnClicked?.Invoke(this, EventArgs.Empty);
+            }
         }
 
         public class MultiplayerWindow : FullScreenTabsWindow
         {
+            private const string BaseLayoutName = "CreditsScreen";
+            private const string SeparatorGameObjectName = "Separator";
+
             public override FullScreenUIType ActiveFullScreenUIType => (FullScreenUIType)555555;
 
             private List<DOTweenAnimation> _animations = new List<DOTweenAnimation>();
 
             private bool _isInitialized = false;
+
+            private MenuItemController _hostMenuController;
+            private MenuItemController _joinMenuController;
 
             public MultiplayerWindow()
             {
@@ -97,15 +139,16 @@ namespace WOTRMultiplayer.Menu
                     return;
                 }
 
+                SetupLayout();
+
                 _isInitialized = true;
                 base.Initialize();
                 IsAnimated = true;
                 var canvas = GetComponent<CanvasGroup>();
                 canvas.alpha = 0f;
                 _animations = GetComponents<DOTweenAnimation>().ToList();
-
                 var closeButton = this.GetComponentInChildren<Owlcat.Runtime.UI.Controls.Button.OwlcatButton>();
-                closeButton.OnLeftClick.AddListener(OnButtonClose);
+                closeButton.OnLeftClick.AddListener(OnCloseClicked);
             }
 
             public override void AppearAnimation()
@@ -137,7 +180,79 @@ namespace WOTRMultiplayer.Menu
             public override void Show(bool state)
             {
                 Logging.Logger.Info($"Opening MP Window. State={state}");
+                _hostMenuController.Activate();
                 base.Show(state);
+            }
+
+            private void OnCloseClicked()
+            {
+                base.OnButtonClose();
+            }
+
+            private void SetupLayout()
+            {
+                var baseLayout = transform.Find(BaseLayoutName)?.gameObject;
+                CleanupBaseLayout(baseLayout);
+
+                var baseMenuItem = SetupBaseMenuItem(baseLayout);
+                _hostMenuController = SetupMenuController(StringsConst.MultiplayerWindow.HostMenuLabel, Screen.width * 0.33f, baseMenuItem, baseLayout.transform);
+                _joinMenuController = SetupMenuController(StringsConst.MultiplayerWindow.JoinMenuLabel, Screen.width * 0.66f, baseMenuItem, baseLayout.transform);
+                UnityEngine.Object.DestroyImmediate(baseMenuItem);
+
+                _hostMenuController.OnClicked += OnHostMenuItemClicked;
+                _joinMenuController.OnClicked += OnJoinMenuItemClicked;
+            }
+
+            private void OnHostMenuItemClicked(object sender, EventArgs e)
+            {
+                _hostMenuController.Activate();
+                _joinMenuController.Deactivate();
+            }
+
+            private void OnJoinMenuItemClicked(object sender, EventArgs e)
+            {
+                _hostMenuController.Deactivate();
+                _joinMenuController.Activate();
+            }
+
+            private void CleanupBaseLayout(GameObject baseLayoutObject)
+            {
+                for (int i = baseLayoutObject.transform.childCount - 1; i >= 0; i--)
+                {
+                    if (i == 1)
+                    {
+                        continue;
+                    }
+
+                    UnityEngine.Object.DestroyImmediate(baseLayoutObject.transform.GetChild(i).gameObject);
+                }
+            }
+
+            private GameObject SetupBaseMenuItem(GameObject baseLayoutObject)
+            {
+                var baseItem = baseLayoutObject.transform.GetChild(0).gameObject;
+
+                UnityEngine.Object.DestroyImmediate(baseItem.GetComponent<OwlcatMultiButton>());
+                baseItem.AddComponent<OwlcatButton>();
+
+                var endSeparator = baseItem.transform.Find(SeparatorGameObjectName);
+                UnityEngine.Object.DestroyImmediate(endSeparator.gameObject);
+
+                return baseItem;
+            }
+
+            private MenuItemController SetupMenuController(string menuItemName, float positionX, GameObject baseMenuItem, Transform parent)
+            {
+                var menuItem = UnityEngine.Object.Instantiate(baseMenuItem, parent);
+                var position = new Vector3(positionX, menuItem.transform.position.y, menuItem.transform.position.z);
+                menuItem.transform.SetPositionAndRotation(position, menuItem.transform.rotation);
+
+                var label = menuItem.GetComponentInChildren<TextMeshProUGUI>();
+                label.SetText(menuItemName);
+
+                var controller = new MenuItemController(menuItem);
+                controller.Initialize();
+                return controller;
             }
         }
     }
