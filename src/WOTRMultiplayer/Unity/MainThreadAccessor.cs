de3228a00d03ffa0faceb5955a6c27eea77333d1
Artem Vasylenko <artemjjke@gmail.com>
2025-08-20 21:38:53 +0300

    Update MainThreadAccessor.cs

--- src/WOTRMultiplayer/Unity/MainThreadAccessor.cs
+++ src/WOTRMultiplayer/Unity/MainThreadAccessor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.Extensions.Logging;
 using UniRx;
 using WOTRMultiplayer.Abstractions.Unity;
 
@@ -7,13 +6,6 @@ namespace WOTRMultiplayer.Unity
 {
     public class MainThreadAccessor : IMainThreadAccessor
     {
-        private readonly ILogger<MainThreadAccessor> _logger;
-
-        public MainThreadAccessor(ILogger<MainThreadAccessor> logger)
-        {
-            _logger = logger;
-        }
-
         public void Post(Action action)
         {
             MainThreadDispatcher.Post(x => action(), null);
