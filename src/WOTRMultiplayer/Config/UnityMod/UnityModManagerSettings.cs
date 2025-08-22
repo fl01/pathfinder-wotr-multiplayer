d54861d373d31eeb4189ff42e9cb7ce38563d4cd
Artem Vasylenko <artemjjke@gmail.com>
2025-08-22 15:55:16 +0300

    Update UnityModManagerSettings.cs

--- src/WOTRMultiplayer/Config/UnityMod/UnityModManagerSettings.cs
+++ src/WOTRMultiplayer/Config/UnityMod/UnityModManagerSettings.cs
@@ -1,13 +1,14 @@
-﻿using UnityModManagerNet;
+﻿using Serilog.Events;
+using UnityModManagerNet;
 
 namespace WOTRMultiplayer.Config.UnityMod
 {
     public class UnityModManagerSettings : UnityModManager.ModSettings
     {
-        public bool UseDebugConsole { get; set; }
+        public bool UseDebugConsole { get; set; } = false;
 
-        public int MinimumLogLevel { get; set; }
+        public int MinimumLogLevel { get; set; } = (int)LogEventLevel.Information;
 
-        public bool AddUnitIdToOvertip { get; set; }
+        public bool AddUnitIdToOvertip { get; set; } = false;
     }
 }
