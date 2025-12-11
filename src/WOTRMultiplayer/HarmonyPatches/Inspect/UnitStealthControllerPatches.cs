37ff064029d800bfe905bfffdf49a527e648af33
Artem Vasylenko <artemjjke@gmail.com>
2025-12-11 09:07:17 +0200

    Update UnitStealthControllerPatches.cs

--- src/WOTRMultiplayer/HarmonyPatches/Inspect/UnitStealthControllerPatches.cs
+++ src/WOTRMultiplayer/HarmonyPatches/Inspect/UnitStealthControllerPatches.cs
@@ -60,32 +60,30 @@ namespace WOTRMultiplayer.HarmonyPatches.Inspect
             var originalDC = ruleCachedPerceptionCheck.OriginalDC;
             var newlyRolledCheck = Rulebook.Trigger(ruleCachedPerceptionCheck);
             // clients don't care about failed rolls
-            if (!newlyRolledCheck.Success)
+            if (newlyRolledCheck.Success)
             {
-                return newlyRolledCheck;
+                // rule resets CachedPerceptionRoll to 0 if it was success, so I see two options here
+                // 1. use base.D20 to get roll -
+                //    it doesn't look very stable, some code paths don't use overriden RollD20 to 'pre-roll' it based on CachedPerceptionRoll
+                //    also I'm lacking understanding if those code paths are actually used in this RuleCachedPerceptionCheck case
+                // 2. force success by using a big number as a roll (e.g. 999)
+                //    looks most promising option as actual value doesn't not really matter if it was a success,
+                //    the only problem is that it will be visibile for a clients in combat log
+                // sticking to option 1 for now, will be replaced with option 2 if needed
+
+                // newlyRolledCheck.D20 -> 999
+                var check = new NetworkStealthPerceptionCheck
+                {
+                    InitiatorId = newlyRolledCheck.Initiator.UniqueId,
+                    StealthedUnitId = unitInStealth.UniqueId,
+                    Roll = newlyRolledCheck.D20,
+                    IsSuccess = newlyRolledCheck.Success,
+                    DC = originalDC,
+                    IgnoreDifficultyBonusToDC = newlyRolledCheck.IgnoreDifficultyBonusToDC,
+                    IsTargetInvisible = newlyRolledCheck.IsTargetInvisible
+                };
+                Main.Multiplayer.OnStealthPerceptionCheckRolled(check);
             }
-
-            // rule resets CachedPerceptionRoll to 0 if it was success, so I see two options here
-            // 1. use base.D20 to get roll -
-            //    it doesn't look very stable, some code paths don't use overriden RollD20 to 'pre-roll' it based on CachedPerceptionRoll
-            //    also I'm lacking understanding if those code paths are actually used in this RuleCachedPerceptionCheck case
-            // 2. force success by using a big number as a roll (e.g. 999)
-            //    looks most promising option as actual value doesn't not really matter if it was a success,
-            //    the only problem is that it will be visibile for a clients in combat log
-            // sticking to option 1 for now, will be replaced with option 2 if needed
-
-            // newlyRolledCheck.D20 -> 999
-            var check = new NetworkStealthPerceptionCheck
-            {
-                InitiatorId = newlyRolledCheck.Initiator.UniqueId,
-                StealthedUnitId = unitInStealth.UniqueId,
-                Roll = newlyRolledCheck.D20,
-                IsSuccess = newlyRolledCheck.Success,
-                DC = originalDC,
-                IgnoreDifficultyBonusToDC = newlyRolledCheck.IgnoreDifficultyBonusToDC,
-                IsTargetInvisible = newlyRolledCheck.IsTargetInvisible
-            };
-            Main.Multiplayer.OnStealthPerceptionCheckRolled(check);
 
             return newlyRolledCheck;
         }
