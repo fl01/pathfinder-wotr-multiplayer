b2af6574d8c67ab39c533a4437c7299434d07fe7
Artem Vasylenko <artemjjke@gmail.com>
2026-02-08 06:17:52 +0200

    use max buff duration in case of overflow

---
+++ src/WOTRMultiplayer/Extensions/MathOperationsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WOTRMultiplayer.Extensions
+{
+    public static class MathOperationsExtensions
+    {
+        public static TimeSpan Add(this TimeSpan timeSpan, TimeSpan other)
+        {
+            try
+            {
+                checked
+                {
+                    return timeSpan + other;
+                }
+            }
+            catch (OverflowException)
+            {
+                return TimeSpan.MaxValue;
+            }
+        }
+    }
+}
--- src/WOTRMultiplayer/Services/GameInteraction/CombatInteractionService.cs
+++ src/WOTRMultiplayer/Services/GameInteraction/CombatInteractionService.cs
@@ -719,8 +719,9 @@ namespace WOTRMultiplayer.Services.GameInteraction
                 {
                     if (!buff.IsPermanent)
                     {
-                        buff.NextResourceSpendingTime = Game.Instance.TimeController.GameTime + networkBuff.NextResourceSpendingTime;
-                        buff.NextTickTime = networkBuff.NextTickTime == TimeSpan.MaxValue ? TimeSpan.MaxValue : Game.Instance.TimeController.GameTime + networkBuff.NextTickTime;
+                        buff.NextResourceSpendingTime = Game.Instance.TimeController.GameTime.Add(networkBuff.NextResourceSpendingTime);
+                        buff.NextTickTime = Game.Instance.TimeController.GameTime.Add(networkBuff.NextTickTime);
+
                         buff.SetDuration(networkBuff.TimeLeft);
                         _logger.LogInformation("Updated buff. UnitId={UnitId}, Id={Id}, Name={Name}, Duration={Duration}, NextResourceSpendingTime={NextResourceSpendingTime}, NextTickTime={NextTickTime}",
                             unit.UniqueId, buff.UniqueId, buff.NameForAcronym, networkBuff.TimeLeft, buff.NextResourceSpendingTime, buff.NextTickTime);
